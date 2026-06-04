074384048d16250d25deb03197c6bdee93c2f6a2
Tomas <felicianoalmeidatomas@gmail.com>
2026-06-04 15:13:58 +0200

    Add connexion function for user authentication

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ localStorage.getItem("mdp")
 let A1=document.getElementById("AN").innerHTML=nom
 let A2=document.getElementById("AG").innerHTML=mdp
 }
+
+
+function connexion(){
+  let noms=prompt("Quel est ton nom?")
+  let mdps=prompt("Quel est ton mot de passe?")
+  if (noms===localStorage.getItem("nom") && mdps===localStorage.getItem("mdp")){
+  alert('Connexion')
+}
+}
